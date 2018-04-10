47e8936949dc8ee825e29fe0935327894728f96d
Marcin Jablonowski <marcin.jab@wp.pl>
2018-04-10 11:21:40 +0200

    Website app

--- pandjango/pandjango/urls.py
+++ pandjango/pandjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from website.views import StartView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', StartView.as_view(), name='index'),
 ]
