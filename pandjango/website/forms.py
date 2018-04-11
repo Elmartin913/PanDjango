975b04b220c1e4c987abf71927d0956889825823
Marcin Jablonowski <marcin.jab@wp.pl>
2018-04-11 22:59:09 +0200

    Contact form - post method

--- pandjango/website/forms.py
+++ pandjango/website/forms.py
@@ -1,27 +1,34 @@
 from django import forms
+from django.core.validators import EmailValidator
 
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
     subject = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 1, 'placeholder': 'Temat'}),
+        widget=forms.TextInput(attrs={'rows': 1, 'placeholder': 'Temat'}),
         max_length=256,
+        required=False,
     )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 3, 'placeholder': 'Wiadomość'}),
+        widget=forms.Textarea(attrs={'rows': 3, 'placeholder': '*Wiadomość'}),
         max_length=1024,
+        required=True,
     )
     name = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 1, 'placeholder': 'Nadawca'}),
+        widget=forms.TextInput(attrs={'rows': 1, 'placeholder': 'Nadawca'}),
         max_length=128,
+        required=False,
     )
     email = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 1, 'placeholder': 'Email'}),
+        widget=forms.TextInput(attrs={'rows': 1, 'placeholder': '*Email'}),
         max_length=128,
+        validators = [EmailValidator()],
+        required=True,
     )
     mobile = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 1, 'placeholder': 'Telefon'}),
+        widget=forms.TextInput(attrs={'rows': 1, 'placeholder': 'Telefon'}),
         max_length=32,
+        required=False,
     )
     class Meta:
 
--- pandjango/website/views.py
+++ pandjango/website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from django.views import View
 from django.template.response import TemplateResponse
@@ -14,11 +15,29 @@ class StartView(View):
 
 class ContactView(View):
     def get(self,request):
-        form = ContactForm
+        form = ContactForm()
         return TemplateResponse(request, 'contact_form.html', {'form':form})
 
     def post(self, request):
-        pass
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            mobile = form.cleaned_data['mobile']
+
+            new_contact = Contact.objects.create(
+                subject=subject,
+                message=message,
+                name=name,
+                email=email,
+                mobile=mobile,
+            )
+
+        return HttpResponse('Wiadomosc wysłana')
+
+
 
 class BoardView(View):
     def get(self, request):
